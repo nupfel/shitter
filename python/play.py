c1fd3269b1305f09cb468a68a0ef5cc25ace46f6
Tobias Kirschstein <lev@ideegeo.com>
2018-07-04 01:02:41 +1200

    playlist cycle

--- python/play.py
+++ python/play.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
-import os
+from os import listdir
+import subprocess
 from time import sleep
 import RPi.GPIO as GPIO
 
 button_pin = 22
 led_pin = 27
+music_dir = '../music'
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN)
@@ -15,6 +17,16 @@ GPIO.setup(led_pin, GPIO.OUT)
 GPIO.output(led_pin, GPIO.HIGH)
 
 state = GPIO.input(button_pin)
+index = 0
+
+wav_files = [ f for f in listdir(music_dir) if f[-4:] == '.wav' ]
+if not (len(wav_files) > 0):
+    print "No files found!"
+    exit()
+
+print '--- Available wav files ---'
+print wav_files
+print '--- Press button to play next file. ---'
 
 while True:
     current_state = GPIO.input(button_pin)
@@ -22,7 +34,11 @@ while True:
     if (current_state != state):
         state = current_state
         GPIO.output(led_pin, GPIO.LOW)
-        os.system('aplay ../music/the\ great\ flush.wav')
+        subprocess.Popen(['aplay', wav_files[index]])
+        print '--- Playing ' + wav_files[index] + ' ---'
         GPIO.output(led_pin, GPIO.HIGH)
+        index += 1
+        if index >= len(wav_files):
+            index = 0
 
     sleep(0.1);
