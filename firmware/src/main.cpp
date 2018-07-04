b6300e88123545f8b5b596c066e9a970faa45225
Tobias Kirschstein <lev@ideegeo.com>
2018-07-04 14:34:58 +1200

    patterns and stuff

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -6,10 +6,10 @@
 
 #define LED_PIN     D1
 #define CHIPSET     WS2812B
+#define WIDTH       8+8+8+6
+#define HEIGHT      37
 #define NUM_LEDS    1110
-
-#define BRIGHTNESS  255
-#define FRAMES_PER_SECOND 200
+#define NUM_PATTERNS 4
 
 // There are two main parameters you can play with to control the look and
 // feel of your fire: COOLING (used in step 1 above), and SPARKING (used
@@ -25,9 +25,32 @@
 // Default 120, suggested range 50-200.
 #define SPARKING 120
 
+uint8_t brightness = 255;
+uint16_t fps = 120;
+bool idle = true;
+long fade_duration = 3000;
+uint8_t pattern_duration_min = 3;
+uint8_t pattern_duration_max = 15;
+uint8_t pattern_duration = random8(pattern_duration_min, pattern_duration_max);
+long last_run = 0;
+
+void handle_serial();
+void show();
+
+// patterns
+void idle_pattern();
+void wave_up();
+void wave_down();
+void wave_left();
+void wave_right();
+void noise();
 void Fire2012WithPalette();
 
 bool gReverseDirection = false;
+
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = { wave_up, wave_down, wave_left, wave_right, noise };
+uint8_t gPatternIndex = random8(NUM_PATTERNS);
 
 CRGB leds[NUM_LEDS];
 
@@ -37,15 +60,13 @@ void setup() {
         delay(1000); // sanity delay
 
         Serial.begin(115200);
-        Serial.println();
-        Serial.println("startup...");
 
-        FastLED.addLeds<CHIPSET, LED_PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-        FastLED.setBrightness( BRIGHTNESS );
+        FastLED.addLeds<CHIPSET, LED_PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+        FastLED.setBrightness(brightness);
 
         // This first palette is the basic 'black body radiation' colors,
         // which run from black to red to bright yellow to white.
-        // gPal = HeatColors_p;
+        gPal = HeatColors_p;
 
         // These are other ways to set up the color palette for the 'fire'.
         // First, a gradient from black to red to yellow to white -- similar to HeatColors_p
@@ -56,13 +77,159 @@ void setup() {
 
         // Third, here's a simpler, three-step gradient, from black to red to white
         // gPal = CRGBPalette16( CRGB::Black, CRGB::Red, CRGB::White);
+
+        Serial.println();
+        Serial.println(HEIGHT);
+        Serial.println(WIDTH);
+        Serial.println(NUM_LEDS);
+        Serial.println("OK");
 }
 
 void loop()
 {
-        // Add entropy to random number generator; we use a lot of it.
-        // random16_add_entropy(random16());
+        handle_serial();
 
+        EVERY_N_SECONDS(1) {
+                Serial.printf("[%d]\n", millis());
+                Serial.printf("\tidle: %d\n", idle);
+                Serial.printf("\tgPatternIndex: %d\n", gPatternIndex);
+                Serial.printf("\tpattern_duration: %d\n", pattern_duration);
+                Serial.printf("\tfps: %d\n", fps);
+        }
+
+        if (idle == true) {
+                idle_pattern();
+        }
+        else {
+                show();
+        }
+
+        FastLED.show();
+        FastLED.delay(1000 / fps);
+}
+
+void handle_serial() {
+        uint8_t cmd;
+
+        if (Serial.available()) {
+                cmd = Serial.read();
+
+                switch (cmd) {
+                case 0:
+                        idle = true;
+                        Serial.println("idle mode");
+                        break;
+                case 1:
+                        idle = false;
+                        last_run = millis();
+                        gPatternIndex = random8(NUM_PATTERNS);
+                        pattern_duration = random8(pattern_duration_min, pattern_duration_max);
+                        Serial.println("show time");
+                        break;
+                default:
+                        Serial.printf("unkown command: %d\n", cmd);
+                        idle = true;
+                }
+
+                // flish the rest if there is any
+                Serial.flush();
+        }
+}
+
+void show() {
+        static uint8_t fade_interval = fade_duration/250;
+        long now = millis();
+
+        // first fade to black
+        if (now - last_run < fade_duration) {
+                EVERY_N_MILLISECONDS(fade_interval) {
+                        // Serial.printf("[%d] fade\n", now); //debug
+                        fadeToBlackBy(leds, NUM_LEDS, 1);
+                }
+                return;
+        }
+
+        gPatterns[gPatternIndex]();
+
+        EVERY_N_SECONDS(pattern_duration) {
+                // switch to random pattern
+                gPatternIndex = random8(NUM_PATTERNS);
+
+                // choose random length for next pattern
+                pattern_duration = random8(pattern_duration_min, pattern_duration_max);
+
+                Serial.printf("switching to pattern index %d for %ds\n", gPatternIndex, pattern_duration);
+        }
+}
+
+void wave_up() {
+        static uint8_t y = 0;
+        static uint8_t colour_index = 0;
+
+        colour_index = (255/(HEIGHT - 1)) * y;
+        Serial.printf("y: %d  c: %d\n", y, colour_index); //debug
+
+        fadeToBlackBy(leds, NUM_LEDS, 10);
+
+        // for (uint8_t x = 0; x < WIDTH; x++) {
+        //         leds[x*HEIGHT+y] = ColorFromPalette(RainbowColors_p, colour_index);
+        // }
+
+        y = (y + 1) % HEIGHT;
+}
+
+void wave_down() {
+        static uint8_t y = HEIGHT - 1;
+        static uint8_t colour_index = 0;
+
+        colour_index = (255/(HEIGHT - 1)) * y;
+        Serial.printf("y: %d  c: %d\n", y, colour_index); //debug
+
+        fadeToBlackBy(leds, NUM_LEDS, 10);
+
+        // for (uint8_t x = 0; x < WIDTH; x++) {
+        //         leds[x*HEIGHT+y] = ColorFromPalette(RainbowColors_p, colour_index);
+        // }
+
+        y = (y - 1) % HEIGHT;
+}
+
+void wave_left() {
+        static uint8_t x = 0;
+        static uint8_t colour_index = 0;
+
+        colour_index = (255/(WIDTH - 1)) * x;
+        Serial.printf("x: %d  c: %d\n", x, colour_index); //debug
+
+        fadeToBlackBy(leds, NUM_LEDS, 10);
+
+        for (uint8_t y = 0; y < HEIGHT; y++) {
+                leds[x*HEIGHT+y] = ColorFromPalette(RainbowColors_p, colour_index);
+        }
+
+        x = (x + 1) % WIDTH;
+}
+
+void wave_right() {
+        static uint8_t x = WIDTH - 1;
+        static uint8_t colour_index = 0;
+
+        colour_index = (255/(WIDTH - 1)) * x;
+        Serial.printf("x: %d  c: %d\n", x, colour_index); //debug
+
+        fadeToBlackBy(leds, NUM_LEDS, 10);
+
+        for (uint8_t y = 0; y < HEIGHT; y++) {
+                leds[x*HEIGHT+y] = ColorFromPalette(RainbowColors_p, colour_index);
+        }
+
+        x = (x - 1) % WIDTH;
+}
+
+void noise() {
+}
+
+void idle_pattern() {
         // Fourth, the most sophisticated: this one sets up a new palette every
         // time through the loop, based on a hue that changes every time.
         // The palette is a gradient from black, to a dark color based on the hue,
@@ -70,22 +237,21 @@ void loop()
         //
         static uint8_t hue = 0;
         hue++;
-        CRGB darkcolor  = CHSV(hue,255,192);   // pure hue, three-quarters brightness
-        CRGB lightcolor = CHSV(hue,128,255);   // half 'whitened', full brightness
-        gPal = CRGBPalette16(CRGB::Black, darkcolor, lightcolor, CRGB::White);
+        CRGB darkcolour  = CHSV(hue,255,192); // pure hue, 3/4 brightness
+        CRGB purecolour = CHSV(hue,255,255);  // pure hue, full brightness
+        CRGB lightcolour = CHSV(hue,200,255); // half 'whitened', full brightness
+        gPal = CRGBPalette32(CRGB::Black, darkcolour, purecolour, lightcolour);
 
         // Fire2012WithPalette(); // run simulation frame, using palette colors
 
-        fadeToBlackBy(leds, NUM_LEDS, 64);
+        fadeToBlackBy(leds, NUM_LEDS, 10);
 
-        if (random8() < 240) {
-                for (uint8_t i = 0; i < 100; i++) {
-                        leds[ random16(NUM_LEDS) ] += ColorFromPalette(gPal, random8());
+        EVERY_N_MILLISECONDS(100) {
+                for (uint8_t i = 0; i < 50; i++) {
+                        leds[ random16(NUM_LEDS) ] = ColorFromPalette(gPal, random8());
                 }
+                // Serial.print("."); //debug
         }
-
-        FastLED.show(); // display this frame
-        FastLED.delay(1000 / FRAMES_PER_SECOND);
 }
 
 
